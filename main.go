6afcc2817ce5be45b98f2afa631d3dafba8709f9
Kamil Szczygieł <kamil.szczygiel@sysdogs.com>
2020-10-22 10:40:09 +0200

    rename fields for datadog parser

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 
 func fastHTTPHandler(ctx *fasthttp.RequestCtx) {
 	if string(ctx.Request.URI().Path()) == "/csp-report" {
-		log.Error().RawJSON("report", ctx.PostBody()).RawJSON("uri", ctx.URI().FullURI()).Send()
+		log.Error().RawJSON("message", ctx.PostBody()).RawJSON("url", ctx.URI().FullURI()).Send()
 	} else {
-		log.Info().RawJSON("uri", ctx.URI().FullURI()).Send()
+		log.Info().RawJSON("url", ctx.URI().FullURI()).Send()
 	}
 }
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.TimestampFieldName = "timestamp"
+
 	log.Info().Msg("Starting HTTP server at 8080")
 	fasthttp.ListenAndServe(":8080", fastHTTPHandler)
 }
